2427806f52c81e852fe8341b4f518aee5e20e889
INDRA-Campinas-at-330432667031 <gab.zer.nas@gmail.com>
2018-09-19 16:04:57 -0400

    Varios elementos

--- m01/learning-props/src/app.js
+++ m01/learning-props/src/app.js
@@ -3,11 +3,18 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+      constructor() {
+            super();
+            this.state = {
+                  value: '2'
+            }
+      }
+      
       render () {
             return (
-                  <div>
-                        My App
-                  </div>
+                  <form>
+                        
+                  </form>
             )
       }
 }
@@ -16,7 +23,74 @@ export default App
 
 
 
-/*================ Lifetype ==================*/
+/*============== select e option ================*/
+// class App extends Component {
+//       constructor() {
+//             super();
+//             this.state = {
+//                   value: '2'
+//             }
+//       }
+      
+//       render () {
+//             return (
+//                   <form>
+//                         <select value={this.state.value} onChange={(e) => {
+//                               this.setState({
+//                                     value: e.target.value
+//                               })
+//                         }}>
+//                               <option value='1'>Opção 1</option>
+//                               <option value='2'>Opção 2</option>
+//                               <option value='3'>Opção 3</option>
+//                         </select>
+//                   </form>
+//             )
+//       }
+// }
+/*================= ##### ===================*/
+
+/*============== Checkbox e radio ================*/
+// import React, { Component } from 'react'
+// class App extends Component {
+//       constructor() {
+//             super();
+//             this.state = {
+//                   value: 'Valor inicial',
+//                   checked: false
+//             }
+//       }      
+//       render () {
+//             return (
+//                   <div>
+//                         <form>
+//                               <input type='text' value={this.state.value} onChange={(e) => {
+//                                     this.setState({
+//                                           value: e.target.value
+//                                     })
+//                               }} />
+//                               <label>
+//                                     <input
+//                                           type='checkbox'
+//                                           value='my-checkbox'
+//                                           checked={this.state.checked}
+//                                           onChange={(e) => {
+//                                                 this.setState({ checked: !this.state.checked })
+//                                           }} />
+//                                     Checkbox
+//                               </label>
+//                               <hr />
+//                               <input type='radio' name='rd' value='1' defaultChecked />Radio 1
+//                               <input type='radio' name='rd' value='2' />Radio 2
+//                         </form>
+//                   </div>
+//             )
+//       }
+// }
+// export default App
+/*================= ##### ===================*/
+
+/*================ Lifecycle ==================*/
 // import React, { Component } from 'react'
 // import Button from './button'
 // import Square from './square'
