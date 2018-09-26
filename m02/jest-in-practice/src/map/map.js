b2d36eb71b4b1838e67db02488f545f5c3cfacdd
INDRA-Campinas-at-330432667031 <gab.zer.nas@gmail.com>
2018-09-26 15:26:40 -0400

    find e excessoes

--- m02/jest-in-practice/src/map/map.js
+++ m02/jest-in-practice/src/map/map.js
@@ -1,6 +1,12 @@
 'use strict'
-
+// with a message "The second parameter must be a function"
 const map = (arr = [], func = (item) => item) => {
+      if (!Array.isArray(arr)) {
+            throw new TypeError('The first parameter must be a array')
+      }
+      if (typeof func !== 'function') {
+            throw new TypeError('The second parameter must be a function')
+      }
       let newArr = []
       for (let i = 0; i < arr.length; i++) {
             newArr.push(func(arr[i], i, arr))
